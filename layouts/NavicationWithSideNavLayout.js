861e33ba72538e26e8a5877b19e5e4ccb01eee8d
Arko <arkoroyb@gmail.com>
2023-01-02 11:23:15 +0600

    test

--- layouts/NavicationWithSideNavLayout.js
+++ layouts/NavicationWithSideNavLayout.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Navication from '../components/Navication/Navication';
 import SideNav from '../components/SideNav/SideNav';
-import Header from '../pages/ProductPage/Header';
 // import styles from '../styles/Home.module.css'
 
 
@@ -9,7 +8,6 @@ const NavicationWithSideNavLayout = ({ children }) => {
     return (
         <main >
             <Navication />
-            <Header />
             <section className={`page-side-nav-wraper my-7 mx-5 xl:mx-12`}>
                 <SideNav />
                 <div className='page-body' id='page-body'>
