66fe4f4e39c82544db692fb3b158c9fb39058030
MdIbrahimKholil7 <kholilmdibrahim17@gmail.com>
2023-01-25 23:43:21 +0600

    category logic add

--- layouts/NavicationWithSideNavLayout.js
+++ layouts/NavicationWithSideNavLayout.js
@@ -1,16 +1,21 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import Navication from '../components/Navication/Navication';
 import SideNav from '../components/SideNav/SideNav';
+import { handleCategorySideNav } from '../features/category/categorySlice';
 // import styles from '../styles/Home.module.css'
 
 
 const NavicationWithSideNavLayout = ({ children }) => {
+    const { category: { isActiveCategory } } = useSelector(state => state || {})
+    const dispatch=useDispatch()
     return (
         <main >
-          
-            <section className={`page-side-nav-wraper my-7 mx-5 xl:mx-12`}>
+
+            <section className={`page-side-nav-wraper my-7 mx-5 xl:mx-12 sideNavTransition`}>
+                <p className='cursor-pointer mb-10' onClick={() => dispatch(handleCategorySideNav())}>Category</p>
                 <SideNav />
-                <div className='page-body' id='page-body'>
+                <div className={`${isActiveCategory ? 'deactive-page-body' : 'active-page-body'} w-[100%-300px]`} id='page-body'>
                     {
                         children
                     }
