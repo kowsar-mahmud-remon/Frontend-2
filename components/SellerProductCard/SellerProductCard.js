8f7acaf3105b1af789a0ff9424ba73a9a9bd8c6b
GaziAshiq <ashiq121914@gmail.com>
2023-01-25 23:48:54 +0600

    desktop view complete

--- components/SellerProductCard/SellerProductCard.js
+++ components/SellerProductCard/SellerProductCard.js
@@ -2,36 +2,49 @@
 import Image from "next/image";
 import React from "react";
 import { FaStar } from "react-icons/fa";
+import { FaShoppingCart } from "react-icons/fa";
 
 const SellerProductCard = ({ product, img1 }) => {
-  const { img, price, pName, rating, sellerVerify } = product;
+  const { img, price, pName, rating, sellerVerify, productOff, OriginalPrice } =
+    product;
 
   return (
     <div>
-      <div className="card w-[222px] bg-base-100 shadow-xl p-3">
-        <figure>
-          <Image className="" src={img} width={170} height={170} alt="Shoes" />
-        </figure>
-        <div className="card-body">
-          <h2 className="card-title text-[#FB641B] font-bold">Tk {price}</h2>
-          <p className="font-bold">{pName}</p>
-          <div className="flex justify-between">
-            <div className="flex bg-[#FB641B] text-white p-1 rounded my-2">
+      <div className="card w-[222px] h-[440px] bg-base-100 shadow-xl">
+        <Image className="" src={img} width="220" height="154" alt="Shoes" />
+
+        <div className="card-body px-[16px] inline">
+          <h2 className="card-title text-[#FB641B] font-bold text-[20px] inline-block mr-[4px] mb-[10px]">
+            Tk {price}{" "}
+          </h2>
+          <span className="text-[#707070] text-[12px]">
+            <span className="line-through">{OriginalPrice}</span>
+            {productOff}
+          </span>
+          <p className="font-bold text-[16px] mb-[10px]">{pName}</p>
+          <div className="flex justify-between ">
+            <div className="flex bg-[#FB641B] text-white  rounded p-[2px] text-[12px] mr-[8px]">
               <span>{rating}</span>
               <FaStar className="my-auto ml-2"></FaStar>
             </div>
-            <span className="text-[13px] text-[#686868] inline-block my-auto ml-2">
+            <span className="text-[13px] text-[#686868] inline-block my-auto ml-2 ">
               24 Ratings & 5 Reviews
             </span>
           </div>
-          <div>
+          <div className="mt-[14px]">
             <Image
               className=""
               src={sellerVerify}
-              width="170"
-              height="170"
+              width="79"
+              height="24"
               alt="logo image"
             />
+            <div className="mt-[38px] mb-[16px]">
+              <button className="btn bg-[#FB641B]">
+                <span className="text-white mr-[4px]">Add to Cart</span>
+                <FaShoppingCart className="text-white h-[20px] w-[24px]"></FaShoppingCart>
+              </button>
+            </div>
           </div>
         </div>
       </div>
--- components/SellerProfile/ProductStore.js
+++ components/SellerProfile/ProductStore.js
@@ -5,7 +5,8 @@ import img2 from "../../assets/images/sellerProfile/product/image 6.png";
 import img3 from "../../assets/images/sellerProfile/product/image 7.png";
 import img4 from "../../assets/images/sellerProfile/product/image 8.png";
 import verified from "../../assets/images/sellerProfile/product/verified.png";
-import Image from "next/image";
+import MoreView from "./MoreView";
+
 //dummy product
 const products = [
   {
@@ -14,7 +15,9 @@ const products = [
     price: "18",
     pName: "Kacha Morich (Green Chili) 250 ±15 gm",
     rating: "3.9",
-    sellerVerify: { verified },
+    sellerVerify: verified,
+    productOff: "(2% off)",
+    OriginalPrice: "Tk 20",
   },
   {
     id: 2,
@@ -22,7 +25,9 @@ const products = [
     price: "160",
     pName: "Tomato (Local) 500 ±30 gm",
     rating: "1.9",
-    sellerVerify: { verified },
+    sellerVerify: verified,
+    productOff: "(20% off)",
+    OriginalPrice: "Tk 80",
   },
   {
     id: 3,
@@ -30,7 +35,7 @@ const products = [
     price: "1024",
     pName: "Misti Kumra Fali (Sweet Pumpkin Fali)",
     rating: "4.9",
-    sellerVerify: { verified },
+    sellerVerify: verified,
   },
   {
     id: 4,
@@ -38,7 +43,9 @@ const products = [
     price: "10018",
     pName: "Misti Kumra Fali (Sweet Pumpkin Fali)",
     rating: "4.9",
-    sellerVerify: { verified },
+    sellerVerify: verified,
+    productOff: "(27% off)",
+    OriginalPrice: "Tk. 1025",
   },
 ];
 
@@ -46,12 +53,12 @@ function ProductStore(props) {
   return (
     <div className="w-[1090px] mx-auto mb-10 mt-[32px]">
       <h3 className="text-[24px] mb-[24px] font-semibold">Products</h3>
-      <div className="flex ">
-        <span className="text-[16px] font-bold">Grocerysss</span>
+      <div className="flex mb-[26px]">
+        <span className="text-[16px] font-bold mr-[8px]">Grocery</span>
         <hr className="text-[#686868] border-t-2 mt-3 w-[958px] h-4 mx-auto" />{" "}
-        <span className="text-[14px]">See More</span>
+        <span className="text-[14px] ml-8px cursor-pointer">See More</span>
       </div>
-      <div className="grid grid-cols-4 w-[960px] mx-auto mb-10">
+      <div className="grid grid-cols-4 w-[960px] mx-auto mb-10 gap-[20px]">
         {products.map((product) => (
           <SellerProductCard
             key={product.id}
@@ -59,10 +66,10 @@ function ProductStore(props) {
           ></SellerProductCard>
         ))}
       </div>
-      <div className="flex ">
-        <span className="text-[12px] font-bold">Kachabazar</span>
-        <hr className="text-[#686868] border-t-2 mt-3 w-[964px] h-4 mx-auto my-4" />{" "}
-        <span className="text-[12px]">See More</span>
+      <div className="flex mb-[26px]">
+        <span className="text-[16px] font-bold mr-[8px]">Kachabazar</span>
+        <hr className="text-[#686868] border-t-2 mt-3 w-[930px] h-4 mx-auto" />{" "}
+        <span className="text-[14px] ml-8px cursor-pointer">See More</span>
       </div>
       <div className="grid grid-cols-4 w-[960px] mx-auto pb-10">
         {products.map((product) => (
@@ -73,6 +80,7 @@ function ProductStore(props) {
           ></SellerProductCard>
         ))}
       </div>
+      <MoreView></MoreView>
     </div>
   );
 }
