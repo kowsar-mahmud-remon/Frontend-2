eb22800281b5852f78fac51fe2129ead38b45c46
Abir Hasan <cse.170201063@gmail.com>
2023-01-26 15:08:42 +0600

    component for sidenavigation bar

---
+++ components/SideNavigationNavBar/SideNavigationNavBar.js
@@ -0,0 +1,77 @@
+import Image from "next/image";
+import { handleCategorySideNav } from "../../features/category/categorySlice";
+import CatIcon from "../../assets/images/Group 2399.png";
+import { useDispatch } from "react-redux";
+
+
+
+const SideNavigationNavBar = () => {
+const dispatch = useDispatch()
+     const options = [
+       {
+         title: "Just For You",
+         total: "",
+       },
+       {
+         title: "FlashSale",
+         total: "",
+       },
+       {
+         title: "Top Offer",
+         total: "300",
+       },
+       {
+         title: "Daily Deals",
+         total: "20",
+       },
+       {
+         title: "Recently Viewed",
+         total: "300",
+       },
+       {
+         title: "Top ranking",
+         total: "",
+       },
+       {
+         title: "New arrivals",
+         total: "",
+       },
+     ];
+    return (
+      <div>
+        <div className="mb-6 flex items-center">
+          <div className="w-72">
+            <button
+              className="cursor-pointer flex justify-center items-center py-3 mr-32 px-6 rounded-full border border-[#FB641B]"
+              onClick={() => dispatch(handleCategorySideNav())}
+            >
+              <Image src={CatIcon} alt="icon" />
+              <p className="ml-3 text-[18px] font-semibold">Categories</p>
+            </button>
+          </div>
+          <div className="ml-5 w-full flex flex-wrap ">
+            {options.map((option, index) => (
+              <div key={index} className="mr-8 mb-3">
+                <button className="px-[30px] py-3 rounded-full shadow-lg ">
+                  <div className="flex justify-center items-center">
+                    <p className="font-[500] text-[17px]">{option.title}</p>
+                    {parseInt(option.total) > 200 ? (
+                      <p className="text-xs font-semibold text-[#FB641B] ml-1">
+                        200+
+                      </p>
+                    ) : (
+                      <p className="text-xs font-semibold text-[#FB641B] ml-1">
+                        {option.total}
+                      </p>
+                    )}
+                  </div>
+                </button>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    );
+};
+
+export default SideNavigationNavBar;
--- layouts/NavicationWithSideNavLayout.js
+++ layouts/NavicationWithSideNavLayout.js
@@ -4,8 +4,8 @@ import Navication from "../components/Navication/Navication";
 import SideNav from "../components/SideNav/SideNav";
 import { handleCategorySideNav } from "../features/category/categorySlice";
 // import styles from '../styles/Home.module.css'
-import CatIcon from "../assets/images/Group 2399.png";
-import Image from "next/image";
+
+import SideNavigationNavBar from "../components/SideNavigationNavBar/SideNavigationNavBar";
 
 const NavicationWithSideNavLayout = ({ children }) => {
   const {
@@ -13,64 +13,13 @@ const NavicationWithSideNavLayout = ({ children }) => {
   } = useSelector((state) => state || {});
   const dispatch = useDispatch();
 
-  const options = [
-    {
-      title: "Just For You",
-      total: "",
-    },
-    {
-      title: "FlashSale",
-      total: "",
-    },
-    {
-      title: "Top Offer",
-      total: "300",
-    },
-    {
-      title: "Daily Deals",
-      total: "20",
-    },
-    {
-      title: "Recently Viewed",
-      total: "300",
-    },
-    {
-      title: "Top ranking",
-      total: "",
-    },
-    {
-      title: "New arrivals",
-      total: "",
-    },
-  ];
+  
   return (
     <main>
       <section
         className={`page-side-nav-wraper my-7 mx-5 xl:mx-12 sideNavTransition`}
       >
-        <div className="mb-6 flex items-center">
-          <div className="w-72">
-            <button
-              className="cursor-pointer flex justify-center items-center py-3 mr-32 px-6 rounded-full border border-[#FB641B]"
-              onClick={() => dispatch(handleCategorySideNav())}
-            >
-              <Image src={CatIcon} alt="icon" />
-              <p className="ml-3 text-[18px] font-semibold">Categories</p>
-            </button>
-          </div>
-          <div className="ml-5 w-full flex flex-wrap ">
-            {options.map((option, index) => (
-              <div key={index} className="mr-8 mb-3">
-                <button className="px-8 py-3 rounded-full shadow-lg ">
-                  <div className="flex justify-center items-center">
-                    <p className="font-[500] text-[18px]">{option.title}</p>
-                    <p className="text-sm text-[#FB641B] ml-1">{option.total}</p>
-                  </div>
-                </button>
-              </div>
-            ))}
-          </div>
-        </div>
+        <SideNavigationNavBar/>
         <SideNav />
         <div
           className={`${
