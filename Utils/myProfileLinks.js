fb8aaeae9dd0aed2a04fb67e83a4d7e77baafabd
MdIbrahimKholil7 <kholilmdibrahim17@gmail.com>
2023-01-30 16:07:42 +0600

    side bar add

---
+++ Utils/myProfileLinks.js
@@ -0,0 +1,80 @@
+import Profile from "../components/manageAccoutSideBar/Profile";
+
+
+export const profileLinksObj = [
+    {
+        title: 'Manage My Account',
+        href: "manageAccount",
+        element:<Profile/>,
+        links: [
+            {
+                title: 'My Profile',
+                href: "myProfile",
+                element: <Profile />
+            },
+            {
+                title: 'Address Book',
+                href: "addressBook",
+                element: <Profile />
+            },
+            {
+                title: 'My Payment Options',
+                href: "myPaymentOptions",
+                element: <Profile />
+            },
+            {
+                title: 'Vouchers',
+                href: "vouchers",
+                element: <Profile />
+            },
+        ]
+    },
+    {
+        title: 'My Orders',
+        href: "myOrders",
+        element:<Profile/>,
+        links: [
+            {
+                title: 'My Returns',
+                href: "myReturns",
+                element: <Profile />
+            },
+            {
+                title: 'My Cancellations',
+                href: "myCancellations",
+                element: <Profile />
+            },
+            {
+                title: 'My Payment Options',
+                href: "myPaymentOptions",
+                element: <Profile />
+            },
+            {
+                title: 'Vouchers',
+                href: "vouchers",
+                element: <Profile />
+            },
+        ]
+    },
+
+    {
+        title: 'My Reviews',
+        href: "myReviews",
+        element: <Profile />
+    },
+    {
+        title: 'My Wishlists',
+        href: "myWishlists",
+        element: <Profile />
+    },
+    {
+        title: 'Followed stores',
+        href: "followedStores",
+        element: <Profile />
+    },
+    {
+        title: 'Sell On Banglar Big Store',
+        href: "sell",
+        element: <Profile />
+    }
+]
