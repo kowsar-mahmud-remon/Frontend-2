75b72b62208911e9d1e5c44606389bcfabd52e97
Abir Hasan <cse.170201063@gmail.com>
2023-02-02 18:27:37 +0600

    position of side buttons

---
+++ components/SideButtons/SideButtons.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const SideButtons = () => {
+  return (
+    <div className="relative">
+      <div className="fixed top-1/2 right-0 z-50 h-screen">
+        <p className="text-red">red</p>
+      </div>
+    </div>
+  );
+};
+
+export default SideButtons;
--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,14 @@
 import { Provider } from 'react-redux'
 import MobileNavBar from '../components/MobileNavBar/MobileNavBar'
 import Navication from '../components/Navication/Navication'
+import SideButtons from '../components/SideButtons/SideButtons'
 import { store } from '../store/store'
 import '../styles/globals.css'
 
 export default function App({ Component, pageProps }) {
   return <div className="">
     <Provider store={store}>
+      <SideButtons/>
       <MobileNavBar/>
       <Navication />
       <Component {...pageProps} />
