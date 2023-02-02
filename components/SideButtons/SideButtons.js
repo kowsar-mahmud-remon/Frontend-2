6196ee773e87673b3cca3f48f92132f465c32a9d
Abir Hasan <cse.170201063@gmail.com>
2023-02-02 19:39:37 +0600

    side button done

--- components/SideButtons/SideButtons.js
+++ components/SideButtons/SideButtons.js
@@ -1,10 +1,21 @@
+import Image from "next/image";
 import React from "react";
-
+import img1 from '../../assets/images/Sidebutton/Chat-logo.png'
+import img2 from '../../assets/images/Sidebutton/vector.png'
 const SideButtons = () => {
   return (
-    <div className="relative">
+    <div className="relative hidden md:block">
       <div className="fixed top-1/2 right-0 z-50 h-screen">
-        <p className="text-red">red</p>
+        <div className="p-2 flex flex-col items-center shadow-lg rounded-xl bg-white mb-2">
+          <Image src={img2} alt="" />
+          <p className="border-b-2">2 Items</p>
+          
+          <p className="text-[#FB641B]">TK. 100</p>
+        </div>
+        <div className="p-2  flex flex-col items-center shadow-lg rounded-xl bg-white">
+          <Image src={img1} alt="" />
+          <p className="text-[#026C51]">Live Chat</p>
+        </div>
       </div>
     </div>
   );
