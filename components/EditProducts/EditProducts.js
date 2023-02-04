5a49984269986bce885f48045ffebd3e2bae19af
creativerze <creativerze@gmail.com>
2023-02-04 22:29:21 +0600

    change some code

--- components/EditProducts/EditProducts.js
+++ components/EditProducts/EditProducts.js
@@ -51,14 +51,16 @@ const EditProducts = () => {
     ];
   return (
     <div className=''>
-      <div className="lg:w-[1426px] mx-auto text-lg text-[#686868] flex items-center mb-2">
+
+      <div className="lg:w-max-[1426px] mx-auto text-lg text-[#686868] flex items-center mb-2">
         <p className='mr-1'>Home</p>
         <FaAngleRight className='text-[#707070] text-xs mr-1'></FaAngleRight>
         <p className='mr-1'>Products</p>
         <FaAngleRight className='text-[#707070] text-xs'></FaAngleRight>
         <p className='text-[#001E00]'>Edit Product</p>
       </div>
-      <div className="lg:w-[1426px] mx-auto">
+
+      <div className="lg:w-max-[1426px] mx-auto">
         <p className=' text-2xl text-[#FB641B] mb-8'>Edit Products</p>
         <div className="flex justify-end">
           <input type="text" placeholder="Product Name" className="input input-bordered w-full max-w-[259px] text-sm ml-4 text-[#686868]" />
@@ -74,25 +76,26 @@ const EditProducts = () => {
         </div>
       </div>
 
-      <div className="lg:w-[1426px] mx-auto mt-6">
+      <div className="lg:w-max-[1426px] mx-auto mt-6">
         <p className='text-[#001E00] text-lg mb-3 font-medium'>Products</p>
-        <div className="p-6 flex flex-wrap justify-center">
+        <div className="p-6 flex flex-wrap lg:justify-center">
           {
             productDetails.map(product => <div key={product._id} className="w-[183px] p-2">
-              <div className=" flex justify-end">
+              <div className=" flex justify-end mb-[-45px] mr-1">
                 <div className="dropdown dropdown-end">
-                  <label tabIndex={0} className="btn m-1">
+                  <label tabIndex={0} className="m-1">
                     <HiDotsHorizontal className='text-[#686868]'></HiDotsHorizontal>
                   </label>
-                  <ul tabIndex={0} className="dropdown-content menu shadow bg-base-100 rounded border w-[121px]">
+                  <ul tabIndex={0} className="dropdown-content menu shadow bg-base-100 rounded border w-[121px] text-base text-[#686868]">
                     <li><a>View Detail</a></li>
                     <li><a>Edit</a></li>
+                    <li><a>Delete</a></li>
+                    <li><a>Copy Link</a></li>
                   </ul>
                 </div>
-
               </div>
               <Image
-                className='w-[167px] h-[126px] p-2 '
+                className='w-[167px] h-[126px] p-2'
                 src={product.img}
                 alt="Picture of the author"
                 width={167}
