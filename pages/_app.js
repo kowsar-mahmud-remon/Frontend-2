a88056c1ab00be3fd4c25293783d1058f5934cbb
Abir Hasan <cse.170201063@gmail.com>
2023-02-04 17:51:10 +0600

    seller right nav created but not implemented

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import { Provider } from 'react-redux'
 import MobileNavBar from '../components/MobileNavBar/MobileNavBar'
 import Navication from '../components/Navication/Navication'
+import SellerRightNav from '../components/sellerRightNav/SellerRightNav'
 import SideButtons from '../components/SideButtons/SideButtons'
 import { store } from '../store/store'
 import '../styles/globals.css'
@@ -8,7 +9,8 @@ import '../styles/globals.css'
 export default function App({ Component, pageProps }) {
   return <div className="">
     <Provider store={store}>
-      <SideButtons/>
+      <SellerRightNav/>
+      {/* <SideButtons/> */}
       <MobileNavBar/>
       <Navication />
       <div className='mr-6'>
