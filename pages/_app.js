9da5a54d6c442ae46ceb84a91bbc9b873992a3f2
Abir Hasan <cse.170201063@gmail.com>
2023-02-05 11:26:17 +0600

    update

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import { useEffect } from 'react'
 import { Provider, useDispatch } from 'react-redux'
 import MobileNavBar from '../components/MobileNavBar/MobileNavBar'
 import Navication from '../components/Navication/Navication'
+import Footer from "../components/ProductPage/Footer";
 import SideButtons from '../components/SideButtons/SideButtons'
 import { store } from '../store/store'
 import '../styles/globals.css'
@@ -9,15 +10,18 @@ import '../styles/globals.css'
 
 export default function App({ Component, pageProps }) {
  
-  return <div className="">
-    <Provider store={store}>
-      <SideButtons/>
-      <MobileNavBar/>
-      <Navication />
-      <div className='mr-6'>
-        <Component {...pageProps} />
-      </div>
-      <Footer />
-    </Provider>
-  </div>
+  return (
+    <div className="">
+      <Provider store={store}>
+        {/* <SellerRightNav/> */}
+        <SideButtons />
+        <MobileNavBar />
+        <Navication />
+        <div className="mr-6">
+          <Component {...pageProps} />
+        </div>
+        <Footer />
+      </Provider>
+    </div>
+  );
 }
