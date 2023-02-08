150fb909c161f89918d3ef9b48c8281e509a0a74
creativerze <creativerze@gmail.com>
2023-02-08 16:39:46 +0600

    added onMouseOver

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,11 @@
-import { useEffect } from 'react'
-import { Provider, useDispatch } from 'react-redux'
-import MobileNavBar from '../components/MobileNavBar/MobileNavBar'
-import Navication from '../components/Navication/Navication'
+import { useEffect } from 'react';
+import { Provider, useDispatch } from 'react-redux';
+import MobileNavBar from '../components/MobileNavBar/MobileNavBar';
+import Navication from '../components/Navication/Navication';
 import Footer from "../components/ProductPage/Footer";
-import SideButtons from '../components/SideButtons/SideButtons'
-import { store } from '../store/store'
-import '../styles/globals.css'
+import SideButtons from '../components/SideButtons/SideButtons';
+import { store } from '../store/store';
+import '../styles/globals.css';
 
 
 export default function App({ Component, pageProps }) {
