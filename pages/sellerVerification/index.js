36dc2d318b81c8b7c2751edc7737854875040f1f
GaziAshiq <ashiq121914@gmail.com>
2023-02-10 00:17:03 +0600

    seller verification page 3 done with responsive

--- pages/sellerVerification/index.js
+++ pages/sellerVerification/index.js
@@ -1,20 +1,9 @@
-import Sellerverification from "../../components/sellerverification/Sellerverification";
-import SecondImage from "../../components/sellerverification/SecondImage";
-import NavicationWithSideNavLayout from "../../layouts/NavicationWithSideNavLayout";
-import SideNavigationNavBar from "../../components/SideNavigationNavBar/SideNavigationNavBar";
-import SellerVerificationPage1 from "../../components/SellerVerificationPage1/SellerVerificationPage1";
-import SellerVerificationPage2 from "../../components/SellerVerificationPage2/SellerVerificationPage2";
+
+import SellerVerification from "../../components/SellerVerificationPage/SellerVerificationPage";
 
 const index = () => {
   return (
-    
-    
-      <SellerVerificationPage2></SellerVerificationPage2>
-      
-      
-        
-       
-    
+      <SellerVerification></SellerVerification>
   );
 };
 
