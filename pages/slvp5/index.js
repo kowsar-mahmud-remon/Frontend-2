416a6d22ea83e39ddf652de57b0804e7ed5c54ba
FHTUSHAR <forhudhossain@gmail.com>
2023-02-11 09:37:10 +0600

    sellerVeriPage5

---
+++ pages/slvp5/index.js
@@ -0,0 +1,10 @@
+import SellerVerificationPage5 from "../../components/SellerVerificationPage5/SellerVerificationPage5";
+
+function slvp() {
+    return (
+        <div>
+            <SellerVerificationPage5></SellerVerificationPage5>
+        </div>
+    )
+}
+export default slvp;
