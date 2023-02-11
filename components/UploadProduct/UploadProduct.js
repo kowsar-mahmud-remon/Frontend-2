f39dcdb0bb9a7a55ce8567b94fe35f2e6339a703
Abir Hasan <cse.170201063@gmail.com>
2023-02-11 14:20:57 +0600

    added

--- components/UploadProduct/UploadProduct.js
+++ components/UploadProduct/UploadProduct.js
@@ -1,6 +1,8 @@
+import Image from "next/image";
 import React from "react";
 import { Radio, RadioGroup } from "react-radio-group";
-
+import img1 from "../../assets/images/uploadproducts/Vector (3).png";
+import img2 from "../../assets/images/uploadproducts/Vector.png";
 const UploadProduct = () => {
   const options = [
     {
@@ -28,9 +30,28 @@ const UploadProduct = () => {
     <div className="p-6">
       <form action="">
         <div className="flex max-w-[1426px] rounded shadow-xl p-6">
-          <div className="w-2/5">{/* image */}</div>
+          <div className="w-2/5">
+            <div className="flex w-full gap-4">
+              <label htmlFor="thumb-btn">
+                <div className="p-36 border-dashed border-2 rounded-lg flex flex-col items-center">
+                  <Image src={img2} alt="" />
+                  <p>Thumbnail Image </p>
+                </div>
+              </label>
+              <input type="file" id="thumb-btn" className="hidden" />
+              <div className="w-">
+                <label htmlFor="thumb-btn">
+                  <div className="py-4 px-1 border-dashed border-2 rounded-lg flex flex-col items-center">
+                    <Image src={img1} alt="" />
+                    <p>Product Video </p>
+                  </div>
+                </label>
+                <input type="file" id="thumb-btn" className="hidden" />
+              </div>
+            </div>
+          </div>
 
-          <div className="w-3/5 ">
+          <div className="w-3/5">
             {/* line 1 */}
             <div className="flex mb-6">
               <label className="mr-4 w-[140px] label justify-end">
