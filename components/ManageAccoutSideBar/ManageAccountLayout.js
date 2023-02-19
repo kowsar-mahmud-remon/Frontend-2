9910c421f6b8e10c1661b0f236161e2b6e908ba8
FHTUSHAR <forhudhossain@gmail.com>
2023-02-19 16:52:07 +0600

    component added

--- components/ManageAccoutSideBar/ManageAccountLayout.js
+++ components/ManageAccoutSideBar/ManageAccountLayout.js
@@ -5,7 +5,7 @@ import ProfileResponsive from "./ProfileResponsive";
 const ManageAccountLayout = ({ children, slug }) => {
 
     return (
-        <div className='lg:flex lg:my-8 lg:justify-center w-full lg:mx-5 lg:gap-[40px] xl:mx-4'>
+        <div className='lg:flex lg:my-8 lg:justify-center md:px-4 w-full lg:mx-5 lg:gap-[40px] xl:mx-4'>
 
             <div className='hidden lg:block'>
 
@@ -19,7 +19,7 @@ const ManageAccountLayout = ({ children, slug }) => {
 
 
             </div>
-            <div>
+            <div className="md:w-[70%]">
                 {
                     children
                 }
