df0d0cd7aa8c2b9a57d2ffebcf11ad33e1b5741f
Abir Hasan <cse.170201063@gmail.com>
2023-02-20 18:58:31 +0600

    issue fixed

--- components/ManageAccoutSideBar/ManageAccountLayout.js
+++ components/ManageAccoutSideBar/ManageAccountLayout.js
@@ -7,7 +7,7 @@ const ManageAccountLayout = ({ children, slug }) => {
     return (
 
         <div className="flex justify-center">
-            <div className='flex gap-5 my-8 w-full'>
+            <div className='md:flex gap-5 my-8 w-full'>
                 <div className='hidden  md:block w-[30%]'>
                     <ManageAccoutSideBar />
                 </div>
