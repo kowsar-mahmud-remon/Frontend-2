297dbe9a4b21a3d6660e846f3456f08bf5303ada
FHTUSHAR <forhudhossain@gmail.com>
2023-02-21 10:52:34 +0600

    responsive done

--- components/ToReceive/ToReceive.js
+++ components/ToReceive/ToReceive.js
@@ -4,9 +4,9 @@ const ToReceive = ({ data }) => {
     return (
         <div className='md:w-[96%]'>
             <div className="mt-[16px] ">
-                <div className="w-[100%] h-[149px] rounded-[8px] bg-[#FFFFFF] shadow-lg pt-[12px]">
+                <div className="w-[100%]  rounded-[8px] bg-[#FFFFFF] shadow-lg pt-[12px]">
                     <div className="flex items-center justify-between">
-                        <div className="mx-[16px] text-[15px] md:text-[16px] ">
+                        <div className="mx-[16px] text-[4.3vw] md:text-[16px] ">
                             <p className='mb-[4px]'><span className=" font-bold text-[#001E00]">Tracking Number</span> <span className="text-[#287DF3]">#{data.orderId}</span></p>
                             <p className='md:hidden  text-[#686868]'>Delivered by:{data.Delivered}</p>
 
@@ -21,18 +21,18 @@ const ToReceive = ({ data }) => {
                                     <Image src={data.image} width="57" height="46" alt=""></Image>
                                 </div>
                                 <div>
-                                    <p className='text-[#001E00] text-[12px] md:text-[18px] font-bold'>{data.peoductName}</p>
+                                    <p className='text-[#001E00] text-[3.4vw] md:text-[18px] font-bold'>{data.peoductName}</p>
                                     <p className='hidden md:block text-[#686868] text-[16px]'>Delivered by:{data.Delivered}</p>
-                                    <p className=" md:hidden block text-[12px] text-[#686868] ">Qty: <span className='text-[#001E00]'>1</span></p>
+                                    <p className=" md:hidden block  text-[3.4vw] text-[#686868] ">Qty: <span className='text-[#001E00]'>1</span></p>
                                 </div>
                             </div>
 
                             <div>
-                                <p className="text-[#287DF3] mt-[29px] text-[14px] md:text-[16px] md:mb-[30px] mr-[19px]">Track Packge</p>
+                                <p className="text-[#287DF3] mt-[29px]  text-[3vw] md:text-[16px] md:mb-[30px] mr-[19px]">Track Packge</p>
 
                             </div>
                         </div>
-                        <p className="text-[#287DF3] md:hidden mt-[-9px] flex justify-end  text-[14px] mb-[12px] mr-[19px]">View More Details</p>
+                        <p className="text-[#287DF3]  md:hidden mt-[-13px] flex justify-end   text-[3vw] pb-[12px] mr-[19px]">View More Details</p>
                     </div>
                 </div>
             </div>
