b8879fc6f9c1fe09cc2602991697772ca55dd701
Abir Hasan <cse.170201063@gmail.com>
2023-02-22 18:15:53 +0600

    upaded links

---
+++ Utils/updatedProfileLink.js
@@ -0,0 +1,100 @@
+const profileSidebarLinks = [
+  {
+    title: "My Profile",
+    href: "myProfile",
+  },
+  {
+    title: "Address Book",
+    href: "addressBook",
+  },
+  {
+    title: "My Payment Options",
+    href: "myPaymentOptions",
+  },
+  {
+    title: "Coupons",
+    href: "Coupons",
+  },
+  {
+    title: "Manage My Account",
+    href: "manageAccount",
+  },
+  {
+    title: "My Returns",
+    href: "myReturns",
+  },
+  {
+    title: "My Cancellations",
+    href: "myCancellations",
+  },
+  {
+    title: "My Orders",
+    href: "myOrders",
+  },
+
+  {
+    title: "My Reviews",
+    href: "myReviews",
+  },
+  {
+    title: "My Wishlists",
+    href: "myWishlists",
+  },
+  {
+    title: "Followed stores",
+    href: "followedStores",
+  },
+  {
+    title: "Sell On Banglar Big Store",
+    href: "sell",
+  },
+];
+
+export const profileResponsiveBar = [
+  {
+    title: "Manage My Account",
+    href: "manageAccount",
+  },
+  {
+    title: "My Profile",
+    href: "manageAccount/myProfile",
+  },
+  {
+    title: "Address book",
+    href: "manageAccount/addressBook",
+  },
+  {
+    title: "My Payment Options",
+    href: "manageAccount/myPaymentOptions",
+  },
+  {
+    title: "My Orders",
+    href: "myOrders",
+  },
+  {
+    title: "My Returns",
+    href: "myReturns",
+  },
+  {
+    title: "My Cancellations",
+    href: "myOrders/myCancellations",
+  },
+  {
+    title: "My Reviews",
+    href: "myReviews",
+  },
+  {
+    title: "My Wishlist",
+    href: "myWishlists",
+  },
+  {
+    title: "Followed Stores",
+    href: "followedStores",
+  },
+  {
+    title: "Sell On Bangla Big Store",
+    href: "sellStore",
+  },
+];
+
+export default profileSidebarLinks;
