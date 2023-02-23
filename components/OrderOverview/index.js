7c62b5194f3ca8f92f506219c494a30aac4ed4ef
kopihannan <kopihannan3@gmail.com>
2023-02-23 20:52:29 +0600

    update

--- components/OrderOverview/index.js
+++ components/OrderOverview/index.js
@@ -36,12 +36,6 @@ const OrderOverview = () => {
                   <table className="w-full text-base text-left text-[#001E00]">
                     <thead className="text-base text-[#001E00] font-normal bg-[#F2F3F7] h-[48px]">
                     <tr>
-                    <th
-                          scope="col"
-                          className=" py-2 font-medium text-[#001E00] text-base"
-                        >
-                          <p className="border-r px-6 border-[#B7B7B7]">Product</p>
-                        </th>
                         <th
                           scope="col"
                           className=" py-2 font-medium text-[#001E00] text-base"
