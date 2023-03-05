c5b9aec90288413fa7ce7dd6a7de12524bdea4ba
Mihir160 <mihir.roy266@gmail.com>
2023-03-05 14:50:08 +0600

    update seller login

--- pages/sellerLogin/login.js
+++ pages/sellerLogin/login.js
@@ -13,94 +13,63 @@ import { useEffect } from 'react';
 import { useCookies } from 'react-cookie';
 import { useRouter } from 'next/router';
 const Login = () => {
-    const [check, setCheck] = useState(false);
-
-    const [loginUser, { data, isLoading, error }] = useLoginUserMutation();
-    const [formError, setError] = useState('');
-    const [cookies, setCookie] = useCookies(['banglarBigStore']);
-    const router = useRouter();
-    useEffect(() => {
-
-        if (data) {
-            setError('');
-            setCookie('banglarBigStore', data?.token, { path: '/' });
-            if (router?.query?.from) router.push(router.query?.from);
-            // router.push('/account/profile')
-        }
-    }, [data, error, cookies, setCookie, router]);
 
 
-    const handleForm = (e) => {
 
-        e.preventDefault();
-
-        if (e.target.email.value.match(/^(?:(?:\+|00)88|01)?\d{11}$/)) {
-            loginUser({
-                phone: e.target.email.value,
-                password: e.target.pass.value
-            });
-        }
-        if (e.target.email.value.match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)) {
-            loginUser({
-                email: e.target.email.value,
-                password: e.target.pass.value
-            });
-        }
-    };
 
     return (
         <NavicationLayout>
-            <div className='  md:mb-[214px] flex  justify-center lg:mx-[20px]'>
+            <div className='  mx-[100px] xl:mx-[209px] mt-[120px]'>
                 <div>
                     <div className='hidden md:block'>
 
-                        <p className='text-[#FB641B] font-semibold text-[16px] md:text-[24px] md:mt-[147px] md:mb-[64px]'>Login Your Banglar Big Store Seller Account</p>
+                        <p className='text-[#FB641B] font-[500] text-[24px]'>Login Your Banglar Big Store Seller Account</p>
                     </div>
-                    <div className='  max-w-[1500px] md:flex  shadow-2xl rounded-2xl bg-white mt-10'>
-                        <div className='px-[28px] md:px-[0px] md:w-[45%] h-auto w-full rounded-md'>
-                            <Image alt="" className='md:h-[784px] md:w-[701px]' src={loginImg} />
-                        </div>
-                        <div className=' md:px-[86px] px-[28px] md:w-[55%] w-full '>
-                            <div className='md:mt-[58px] md:block hidden'>
-                                <Image alt="" src={logo} />
-
+                    <div className='  max-w-[1500px] mt-[32px]  h-[652px] shadow-small '>
+                        <div className='flex  justify-center  gap-[49px]'>
+                            <div className='mt-[143px]'>
+                                <Image alt="" className='h-[366px] w-[453px]' src={loginImg} />
                             </div>
-                            <form
-                                onSubmit={handleForm}
-                                className=' flex flex-col md:mt-[32px]'>
-                                <label className='text-[24px] text-[#FB641B] md:mb-[12px] mt-[20px] font-semibold'>Login</label>
-                                <p className='block  md:hidden sm:hidden mt-[24px] mb-2 text-[14px] text-[#001E00]'>Email Or Phone Number*</p>
-                                <input
-                                    name='email'
-                                    className='input border focus:outline-none border-[#707070] md:mb-[32px] mb-[20px] bg-white h-[48px]  md:h-[72px] md:text-[16px]' type="text" placeholder='Enter Your Phone Number or Email' />
-                                {
-                                    formError && <p className='text-red-500 text-[15px] '>{formError}</p>
-                                }
-                                <p className='block  md:hidden sm:hidden mt-[20px] mb-2 text-[14px]  text-[#001E00]'>Password*</p>
-                                <input
-                                    name='pass'
-                                    className='input focus:outline-none border border-[#707070]  md:h-[72px] md:text-[16px]' type={check ? 'text' : 'password'} placeholder='******' />
-                                <div className='flex justify-between mt-[12px] mb-[20px]'>
-                                    <div className='flex gap-[12px] '>
-                                        <input className='h[19px]  w-[19px] border border-[#686868]' onChange={e => setCheck(e.target.checked)} type="checkbox" />
-                                        <p className='text-[#686868] font-[400] text-[16px]'>Show Passwords</p>
-                                    </div>
-                                    <Link href="/sellerLogin/forgetPassword">  <p className='text-[#287DF3] text-[16px] font font-[400] '>Forgot Password?</p></Link>
+                            <div className=' '>
+                                <div className='mt-[68px]'>
+                                    <Image alt="" src={logo} />
+                                    <h1 className='text-[#FB641B] text-[24px] mt-[18px]'>Login</h1>
                                 </div>
+                                <form className='mt-[12px]'>
+                                    <div>
+                                        <input
+                                            name='email'
+                                            className='xxl:w-[614px]   w-full border-solid focus:outline-none border-[#287DF3] border-[1px] hover:shadow-small rounded-[8px] h-[72px] px-[25px] placeholder:text-[1vw] placeholder:text-[#707070]' type="text" placeholder='Enter Your Phone Number or Email' />
+                                    </div>
+                                    <div>
+                                        <input
+                                            name='password'
+                                            className='xxl:w-[614px]  mt-[32px] w-full border-solid focus:outline-none border-[#287DF3] border-[1px] hover:shadow-small rounded-[8px] h-[72px] px-[25px] placeholder:text-[1vw] placeholder:text-[#707070]' type="password" placeholder='*****' />
+                                    </div>
+                                    <div className='flex justify-between mt-[12px] mb-[20px]'>
+                                        <div className='flex gap-[12px] '>
+                                            <input className='h[19px] w-[19px] border border-[#686868]' type="checkbox" />
+                                            <p className='text-[#686868]  text-[16px]'>Show Passwords</p>
+                                        </div>
+                                        <Link href="/sellerLogin/forgetPassword">
+                                            <p className='text-[#287DF3] text-[16px]'>Forgot Password?</p></Link>
+                                    </div>
 
-                                {
-                                    error?.data?.message && <p className='text-red-500 text-[19px] mb-2 text-center '>{error?.data?.message}</p>
-                                }
 
-                                <Link href="/sellerLogin/sellerOtp">
-                                    <button className='btn bg-[#FB641B] text-[18px] md:h-[72px]  text-white md:mt-[25px]'>Login</button></Link>
-                                <span className='mx-auto text-[#FB641B] md:mt-[8px] text-[24px] mt-[20px]'>Or</span>
-
-                            </form>
+                                    <Link href="/sellerLogin/sellerOtp">
+                                        <button className='btn bg-[#FB641B] text-[18px] md:h-[72px]  text-white md:mt-[25px]'>Login</button>
+                                    </Link>
+                                    <div className='flex justify-center'>
+                                        <span className='mx-auto text-[#FB641B] md:mt-[8px] text-[24px]  mt-[20px]'>Or</span>
+                                    </div>
 
 
-                            <div className='flex flex-col gap-6 md:mt-3  pb-[145px] md:pb-[50px] '>
-                                <p className='mx-auto font-[400] md:text-[18px] text-[16px] text-[#001E00]'>You have no Seller account? <Link className='text-[#287DF3]' href='/registation'>Register</Link></p>
+                                </form>
+
+
+                                <div className='flex flex-col gap-6 md:mt-3  pb-[145px] md:pb-[50px] '>
+                                    <p className='mx-auto font-[400] md:text-[18px] text-[16px] text-[#001E00]'>You have no Seller account? <Link className='text-[#287DF3]' href='/sellerLogin/sellerRegister'>Register</Link></p>
+                                </div>
                             </div>
                         </div>
                     </div>
---
+++ pages/sellerLogin/sellerRegister.js
@@ -0,0 +1,63 @@
+import Link from "next/link";
+import NavicationLayout from "../../layouts/NavicationLayout";
+
+
+const sellerRegister = () => {
+    return (
+        <div>
+            <NavicationLayout>
+                <div className="flex justify-center">
+                    <div className="mt-[139px]">
+                        <div className="flex justify-between">
+                            <div>
+                                <h1 className="text-[#FB641B] text-[24px] font-[500]">Create an Seller Account</h1>
+                                <h2 className="text-[#686868] text-[16px] mt-[8px]">Welcome! Our Banglar Big Store, Users are waiting <br /> to buy your product.</h2>
+                            </div>
+                            <div>
+                                <p className='mx-auto mt-[5px] font-[400] md:text-[18px] text-[16px] text-[#686868]'>Already have an account? <Link className='text-[#287DF3]' href='/sellerLogin/login'>Login</Link></p>
+                            </div>
+                        </div>
+
+                        <div className="w-[742px] h-[697px] shadow-small rounded-[8px] mt-[16px]">
+                            <div className="mx-[64px]">
+                                <form className="pt-[72px]">
+                                    <div>
+                                        <h1 className="text-[#001E00] text-[16px] font-[400]">Phone Number*</h1>
+                                        <input
+                                            name='email'
+                                            className='xxl:w-[614px]   w-full border-solid mt-[8px] focus:outline-none border-[#B7B7B7] border-[1px] hover:shadow-small rounded-[8px] h-[72px] px-[25px] shadow-small placeholder:text-[16px] placeholder:text-[#707070]' type="text" placeholder='Please Provide your phone number' />
+                                    </div>
+                                    <div className="mt-[40px]">
+                                        <h1 className="text-[#001E00] text-[16px] font-[400]">Password*</h1>
+                                        <input
+                                            name='password'
+                                            className='xxl:w-[614px]   w-full border-solid mt-[8px] focus:outline-none border-[#B7B7B7] border-[1px] hover:shadow-small rounded-[8px] h-[72px] px-[25px] shadow-small placeholder:text-[16px] placeholder:text-[#707070]' type="password" placeholder='Minimum 6 Characters with a number And a letter' />
+
+                                        <div className='flex gap-[12px] mt-[12px]'>
+                                            <input className='h[19px] w-[19px] border border-[#686868]' type="checkbox" />
+                                            <p className='text-[#686868]  text-[16px]'>Show Passwords</p>
+                                        </div>
+                                    </div>
+                                    <div className="mt-[40px]">
+                                        <h1 className="text-[#001E00] text-[16px] font-[400]">Conform Password*</h1>
+                                        <input
+                                            name='password'
+                                            className='xxl:w-[614px]   w-full border-solid mt-[8px] focus:outline-none border-[#B7B7B7] border-[1px] hover:shadow-small rounded-[8px] h-[72px] px-[25px] shadow-small placeholder:text-[16px] placeholder:text-[#707070]' type="password" placeholder='Minimum 6 Characters with a number And a letter' />
+
+                                        <div className='flex gap-[12px] mt-[12px]'>
+                                            <input className='h[19px] w-[19px] border border-[#686868]' type="checkbox" />
+                                            <p className='text-[#686868]  text-[16px]'>Show Passwords</p>
+                                        </div>
+                                    </div>
+                                    <button className='btn bg-[#FB641B]  text-[18px] md:h-[72px]  text-white md:mt-[25px]'>Create an Account</button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </NavicationLayout>
+        </div>
+    );
+};
+
+export default sellerRegister;
