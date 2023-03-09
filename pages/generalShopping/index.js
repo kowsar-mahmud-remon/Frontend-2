a10daf53a57529c8cdbbf026d56e9e6b2148c4a4
Programmer Bikash <bikash19081998dey@gmail.com>
2023-03-09 21:26:36 +0600

    banner created

---
+++ pages/generalShopping/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import TopSection from '../../components/GeneralShopping/TopSection';
+
+const generalShopping = () => {
+    return (
+        <div>
+            <TopSection />
+        </div>
+    );
+};
+
+export default generalShopping;
