d9258a952ce01eee4569e6a9bb1ef75ece389132
FHTUSHAR <forhudhossain@gmail.com>
2023-03-10 15:20:11 +0600

    just for you

---
+++ pages/generalShopping/index.js
@@ -0,0 +1,8 @@
+import JustForYou from "../../components/JustForYou/JustForYou"
+
+function generalShopping() {
+    return (
+        <JustForYou></JustForYou>
+    )
+}
+export default generalShopping
