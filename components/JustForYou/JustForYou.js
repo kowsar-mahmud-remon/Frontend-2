012adc7851f462a42ae75b40502a974d80310c8d
FHTUSHAR <forhudhossain@gmail.com>
2023-03-11 10:14:01 +0600

    just for you done

--- components/JustForYou/JustForYou.js
+++ components/JustForYou/JustForYou.js
@@ -73,20 +73,20 @@ const JustForYou = () => {
     return (
         <div className=' w-[90%] mx-auto'>
             <div className='mx-auto '>
-                <h1 className='text-[1.6vw] mb-[16px] font-[700]'> Just For You</h1>
+                <h1 className='md:text-[1.6vw] mb-[16px] font-[700]'> Just For You</h1>
 
-                <div className='flex gap-[16px] flex-wrap '>
+                <div className='md:flex gap-[16px] md:flex-wrap '>
                     {
                         datas.map(data => {
                             return (
-                                <div key={data.id} className='bg-white w-[16.2vw] h-auto rounded-[10px]'>
+                                <div key={data.id} className='bg-white md:w-[16.2vw] h-auto rounded-[10px]'>
                                     <Image className=' h-auto rounded-t-[10px]'
                                         src={data.image}
                                         alt='Image' />
                                     <div className='p-[16px]'>
-                                        <p className='text-[1.2vw] text-[#001E00] mb-[12px]'>{data.details}</p>
-                                        <p className='text-[1.4vw] font-[700] mb-[4px]'>{data.price}</p>
-                                        <p className='text-[1.2vw] text-[#001E00]'>MOQ: {data.qunatity} pieces</p>
+                                        <p className='md:text-[1.2vw] text-[#001E00] mb-[12px]'>{data.details}</p>
+                                        <p className='md:text-[1.4vw] font-[700] mb-[4px]'>{data.price}</p>
+                                        <p className='md:text-[1.2vw] text-[#001E00]'>MOQ: {data.qunatity} pieces</p>
                                     </div>
                                 </div>
                             )
