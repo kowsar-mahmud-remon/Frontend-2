407570c13d8b0c1520a9ae232b9c154faef95320
Abir Hasan <cse.170201063@gmail.com>
2023-03-16 22:27:00 +0600

    mobile version cash on delivery done

--- components/myOrderDetails/Timeline/Timeline.js
+++ components/myOrderDetails/Timeline/Timeline.js
@@ -141,45 +141,90 @@ const Timeline = ({ status, payOption }) => {
           </div>
         </div>
         {/* mobile */}
-        <div className="lg:hidden flex h-[260px]">
-          <div className="flex flex-col justify-center items-center  my-1">
-            <div className="h-full w-1 my-1 bg-[#F2F2F2]">
-              <div className="w-full h-1/2 bg-[#026C51] "></div>
+        {payOption == "Cash On Delivery" ? (
+          <div className="lg:hidden flex h-[260px]">
+            <div className="flex flex-col justify-center items-center  my-1">
+              <div className="h-full w-1 my-1 bg-[#F2F2F2]">
+                <div className="w-full h-[55%] bg-[#026C51] "></div>
+              </div>
             </div>
-          </div>
-          <div className="relative w-full">
-            <div className="flex flex-col justify-between h-[260px]  absolute  -left-2 w-full">
-              <div>
-                <div className="flex items-center">
-                  <div className="w-3 h-3 rounded-full border-2 border-[#026C51] bg-[#026C51]"></div>
-                  <p className="ml-2">Processing</p>
-                </div>
-                <div className="">
-                  <div className=" h-[96px] p-2 bg-[#F2F3F7] rounded mt-3 relative text-[12px] ml-6 ">
-                    <div className="w-3 h-3 bg-[#F2F3F7] absolute -top-1 -z-10 left-4 rotate-45"></div>
-                    {/* eta dynamic hobe */}
-                    <p>
-                      31 Dec 2022 - 16:52{" "}
-                      <span className="font-[500]">
-                        Your order has been successfully verified
-                      </span>
-                    </p>
-                    <p className="text-[#287DF3] ">View More</p>
-                  </div>
+            <div className="relative w-full">
+              <div className="flex flex-col justify-between h-[260px]  absolute  -left-2 w-full">
+                <div>
                   <div className="flex items-center">
                     <div className="w-3 h-3 rounded-full border-2 border-[#026C51] bg-[#026C51]"></div>
-                    <p className="ml-2">Shipping</p>
+                    <p className="ml-2">Processing</p>
+                  </div>
+                  <div className="">
+                    <div className=" h-[96px] p-2 bg-[#F2F3F7] rounded mt-3 relative text-[12px] ml-6 ">
+                      <div className="w-3 h-3 bg-[#F2F3F7] absolute -top-1 -z-10 left-4 rotate-45"></div>
+                      {/* eta dynamic hobe */}
+                      <p>
+                        31 Dec 2022 - 16:52{" "}
+                        <span className="font-[500]">
+                          Your order has been successfully verified
+                        </span>
+                      </p>
+                      <p className="text-[#287DF3] ">View More</p>
+                    </div>
+                    <div className="flex items-center">
+                      <div className="w-3 h-3 rounded-full border-2 border-[#026C51] bg-[#026C51]"></div>
+                      <p className="ml-2">Shipping</p>
+                    </div>
                   </div>
                 </div>
-              </div>
 
-              <div className="flex items-center">
-                <div className="w-3 h-3 rounded-full border-2 border-[#F2F2F2] bg-[#F2F2F2]"></div>
-                <p className="ml-2">Delivered</p>
+                <div className="flex items-center">
+                  <div className="w-3 h-3 rounded-full border-2 border-[#F2F2F2] bg-[#F2F2F2]"></div>
+                  <p className="ml-2">Delivered</p>
+                </div>
               </div>
             </div>
           </div>
-        </div>
+        ) : (
+          <div className="lg:hidden flex h-[320px]">
+            <div className="flex flex-col justify-center items-center  my-1">
+              <div className="h-full w-1 my-1 bg-[#F2F2F2]">
+                <div className="w-full h-[73%] bg-[#026C51] "></div>
+              </div>
+            </div>
+            <div className="relative w-full">
+              <div className="flex flex-col justify-between h-[320px]  absolute  -left-2 w-full">
+                <div>
+                  <div className="flex items-center">
+                    <div className="w-3 h-3 rounded-full border-2 border-[#026C51] bg-[#026C51]"></div>
+                    <p className="ml-2">Payment Success</p>
+                  </div>
+                  <div className="">
+                    <div className=" h-[96px] p-2 bg-[#F2F3F7] rounded mt-3 relative text-[12px] ml-6 ">
+                      <div className="w-3 h-3 bg-[#F2F3F7] absolute -top-1 -z-10 left-4 rotate-45"></div>
+                      {/* eta dynamic hobe */}
+                      <p>
+                        31 Dec 2022 - 16:52{" "}
+                        <span className="font-[500]">
+                          Your order has been successfully verified
+                        </span>
+                      </p>
+                      <p className="text-[#287DF3] ">View More</p>
+                    </div>
+                    <div className="flex items-center">
+                      <div className="w-3 h-3 rounded-full border-2 border-[#026C51] bg-[#026C51]"></div>
+                      <p className="ml-2">Processing</p>
+                    </div>
+                  </div>
+                </div>
+                <div className="flex items-center">
+                  <div className="w-3 h-3 rounded-full border-2 border-[#026C51] bg-[#026C51]"></div>
+                  <p className="ml-2">Shipping</p>
+                </div>
+                <div className="flex items-center">
+                  <div className="w-3 h-3 rounded-full border-2 border-[#F2F2F2] bg-[#F2F2F2]"></div>
+                  <p className="ml-2">Delivered</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        )}
       </div>
     );
   }
@@ -235,45 +280,90 @@ const Timeline = ({ status, payOption }) => {
         </div>
 
         {/* mobile */}
-        <div className="lg:hidden flex h-[260px]">
-          <div className="flex flex-col justify-center items-center  my-1">
-            <div className="h-full w-1 my-1">
-              <div className="w-full h-full bg-[#026C51] "></div>
+        {payOption == "Cash On Delivery" ? (
+          <div className="lg:hidden flex h-[260px]">
+            <div className="flex flex-col justify-center items-center  my-1">
+              <div className="h-full w-1 my-1">
+                <div className="w-full h-full bg-[#026C51] "></div>
+              </div>
             </div>
-          </div>
-          <div className="relative w-full">
-            <div className="flex flex-col justify-between h-[260px]  absolute  -left-2 w-full">
-              <div>
-                <div className="flex items-center">
-                  <div className="w-3 h-3 rounded-full border-2 border-[#026C51] bg-[#026C51]"></div>
-                  <p className="ml-2">Processing</p>
-                </div>
-                <div className="">
-                  <div className=" h-[96px] p-2 bg-[#F2F3F7] rounded mt-3 relative text-[12px] ml-6 ">
-                    <div className="w-3 h-3 bg-[#F2F3F7] absolute -top-1 -z-10 left-4 rotate-45"></div>
-                    {/* eta dynamic hobe */}
-                    <p>
-                      31 Dec 2022 - 16:52{" "}
-                      <span className="font-[500]">
-                        Your order has been successfully verified
-                      </span>
-                    </p>
-                    <p className="text-[#287DF3] ">View More</p>
-                  </div>
+            <div className="relative w-full">
+              <div className="flex flex-col justify-between h-[260px]  absolute  -left-2 w-full">
+                <div>
                   <div className="flex items-center">
                     <div className="w-3 h-3 rounded-full border-2 border-[#026C51] bg-[#026C51]"></div>
-                    <p className="ml-2">Shipping</p>
+                    <p className="ml-2">Processing</p>
+                  </div>
+                  <div className="">
+                    <div className=" h-[96px] p-2 bg-[#F2F3F7] rounded mt-3 relative text-[12px] ml-6 ">
+                      <div className="w-3 h-3 bg-[#F2F3F7] absolute -top-1 -z-10 left-4 rotate-45"></div>
+                      {/* eta dynamic hobe */}
+                      <p>
+                        31 Dec 2022 - 16:52{" "}
+                        <span className="font-[500]">
+                          Your order has been successfully verified
+                        </span>
+                      </p>
+                      <p className="text-[#287DF3] ">View More</p>
+                    </div>
+                    <div className="flex items-center">
+                      <div className="w-3 h-3 rounded-full border-2 border-[#026C51] bg-[#026C51]"></div>
+                      <p className="ml-2">Shipping</p>
+                    </div>
                   </div>
                 </div>
-              </div>
 
-              <div className="flex items-center">
-                <div className="w-3 h-3 rounded-full border-2 border-[#026C51] bg-[#026C51]"></div>
-                <p className="ml-2">Delivered</p>
+                <div className="flex items-center">
+                  <div className="w-3 h-3 rounded-full border-2 border-[#026C51] bg-[#026C51]"></div>
+                  <p className="ml-2">Delivered</p>
+                </div>
               </div>
             </div>
           </div>
-        </div>
+        ) : (
+          <div className="lg:hidden flex h-[320px]">
+            <div className="flex flex-col justify-center items-center  my-1">
+              <div className="h-full w-1 my-1">
+                <div className="w-full h-full bg-[#026C51] "></div>
+              </div>
+            </div>
+            <div className="relative w-full">
+              <div className="flex flex-col justify-between h-[320px]  absolute  -left-2 w-full">
+                <div>
+                  <div className="flex items-center">
+                    <div className="w-3 h-3 rounded-full border-2 border-[#026C51] bg-[#026C51]"></div>
+                    <p className="ml-2">Payment Success</p>
+                  </div>
+                  <div className="">
+                    <div className=" h-[96px] p-2 bg-[#F2F3F7] rounded mt-3 relative text-[12px] ml-6 ">
+                      <div className="w-3 h-3 bg-[#F2F3F7] absolute -top-1 -z-10 left-4 rotate-45"></div>
+                      {/* eta dynamic hobe */}
+                      <p>
+                        31 Dec 2022 - 16:52{" "}
+                        <span className="font-[500]">
+                          Your order has been successfully verified
+                        </span>
+                      </p>
+                      <p className="text-[#287DF3] ">View More</p>
+                    </div>
+                    <div className="flex items-center">
+                      <div className="w-3 h-3 rounded-full border-2 border-[#026C51] bg-[#026C51]"></div>
+                      <p className="ml-2">Processing</p>
+                    </div>
+                  </div>
+                </div>
+                <div className="flex items-center">
+                  <div className="w-3 h-3 rounded-full border-2 border-[#026C51] bg-[#026C51]"></div>
+                  <p className="ml-2">Shipping</p>
+                </div>
+                <div className="flex items-center">
+                  <div className="w-3 h-3 rounded-full border-2 border-[#026C51] bg-[#026C51]"></div>
+                  <p className="ml-2">Delivered</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        )}
       </div>
     );
   }
