c23ebfdd16c6f3d0530ca8d2c9a2ceba0547c159
FHTUSHAR <forhudhossain@gmail.com>
2023-03-20 13:05:27 +0600

    debit responsive and desktop done

---
+++ components/CreditMobile/CreditMobile.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CreditMobile = () => {
+    return (
+        <div className='px-[24px]'>
+            <h1>PayMent Method</h1>
+        </div>
+    );
+};
+
+export default CreditMobile;
